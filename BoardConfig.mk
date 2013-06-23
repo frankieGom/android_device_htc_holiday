2ab79f30294834a5654faf7a1b5d24e9ef48e55d
Matt Filetto <matt.filetto@gmail.com>
2013-06-22 17:38:42 -0700

    BoardConfig: cleanup
    Change-Id: I2a524190519d5ca6c0acba9c9e63869e882e44ed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,9 +36,12 @@ BOARD_PAGE_SIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=holiday no_console_suspend=1
 
 # Kernel [Build]
-TARGET_KERNEL_CONFIG := sultan_defconfig
+TARGET_KERNEL_CONFIG := holiday_defconfig
 BUILD_KERNEL := true
 
+# ril
+BOARD_USE_NEW_LIBRIL_HTC := true
+BOARD_USES_LEGACY_RIL := true
 # Use libril in the device tree
 BOARD_PROVIDES_LIBRIL := true
 
